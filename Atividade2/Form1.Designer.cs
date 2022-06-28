f5b375c0efe24b39dcae461c60c8ba080be5eeae
VictorHMarcondes <108373750+VictorHMarcondes@users.noreply.github.com>
2022-06-28 20:10:39 -0300

    Initial commit

---
+++ Atividade2/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Atividade2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNOME = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.btnCAL = new System.Windows.Forms.Button();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.txtIMC = new System.Windows.Forms.TextBox();
+            this.lblIMC = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblNOME
+            // 
+            this.lblNOME.AutoSize = true;
+            this.lblNOME.Location = new System.Drawing.Point(54, 49);
+            this.lblNOME.Name = "lblNOME";
+            this.lblNOME.Size = new System.Drawing.Size(31, 13);
+            this.lblNOME.TabIndex = 0;
+            this.lblNOME.Text = "Peso";
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Location = new System.Drawing.Point(54, 75);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(34, 13);
+            this.lblAltura.TabIndex = 1;
+            this.lblAltura.Text = "Altura";
+            // 
+            // btnCAL
+            // 
+            this.btnCAL.Location = new System.Drawing.Point(105, 112);
+            this.btnCAL.Name = "btnCAL";
+            this.btnCAL.Size = new System.Drawing.Size(75, 23);
+            this.btnCAL.TabIndex = 2;
+            this.btnCAL.Text = "CALCULAR";
+            this.btnCAL.UseVisualStyleBackColor = true;
+            this.btnCAL.Click += new System.EventHandler(this.btnCAL_Click);
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(91, 46);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(100, 20);
+            this.txtPeso.TabIndex = 3;
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(91, 72);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(100, 20);
+            this.txtAltura.TabIndex = 4;
+            // 
+            // txtIMC
+            // 
+            this.txtIMC.Location = new System.Drawing.Point(91, 155);
+            this.txtIMC.Name = "txtIMC";
+            this.txtIMC.Size = new System.Drawing.Size(100, 20);
+            this.txtIMC.TabIndex = 5;
+            // 
+            // lblIMC
+            // 
+            this.lblIMC.AutoSize = true;
+            this.lblIMC.Location = new System.Drawing.Point(50, 162);
+            this.lblIMC.Name = "lblIMC";
+            this.lblIMC.Size = new System.Drawing.Size(26, 13);
+            this.lblIMC.TabIndex = 6;
+            this.lblIMC.Text = "IMC";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(57, 200);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 13);
+            this.lblResultado.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblIMC);
+            this.Controls.Add(this.txtIMC);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.btnCAL);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.lblNOME);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNOME;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.Button btnCAL;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.TextBox txtIMC;
+        private System.Windows.Forms.Label lblIMC;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ Atividade2/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atividade2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCAL_Click(object sender, EventArgs e)
+        {
+            // iniciando variavel com valor 0
+            double peso = 0;
+            double altura = 0;
+            double imc= 0;
+            //passando valor pra variavel
+            if (txtPeso.Text != "" && txtAltura.Text != "")
+            {
+               peso = double.Parse(txtPeso.Text);
+               altura = double.Parse(txtAltura.Text);
+                
+            }
+            //CALCULANDO IMC
+            if(peso !=0)
+            {
+                imc = peso / (altura * altura) ;
+            }
+
+            //Classificando IMC
+            if (imc < 18.5)
+            {
+                lblResultado.Text = "Abaixo do peso";
+                lblResultado.ForeColor = Color.Red;
+            }
+            else if(imc >= 18.6 && imc <= 24.9)
+            {
+                lblResultado.Text = "Peso ideal" ;
+                lblResultado.ForeColor = Color.Green;
+            }
+            else if (imc >= 25 && imc <= 29.9)
+            {
+                lblResultado.Text = "levemente acima do peso";
+                lblResultado.ForeColor = Color.Red;
+            }
+             else if (imc >= 30 && imc <= 34.9)
+            {
+                lblResultado.Text = "Obesidade grau I";
+                lblResultado.ForeColor = Color.Red;
+            }
+             else if (imc >= 35 && imc <= 39.9)
+            {
+                lblResultado.Text = "Obesidade grau II";
+                lblResultado.ForeColor = Color.Red;
+            }
+             else if (imc >= 40 )
+            {
+                lblResultado.Text = "Obesidade grau III";
+                lblResultado.ForeColor = Color.Red;
+            }
+
+            //mostrar o valor:
+            txtIMC.Text = imc.ToString();
+        }
+    }
+}
